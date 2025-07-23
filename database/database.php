5c0539c5edb1a209b9d657a0cc6177d2116990e6
briyanhs <briyanadobestock@gmail.com>
2025-07-23 10:43:18 +0700

    ganti_database

--- database/database.php
+++ database/database.php
@@ -2,7 +2,7 @@
 $host_db = "localhost";
 $user_db = "root";
 $pass_db = "";
-$nama_db = "renbang";
+$nama_db = "laboratorium";
 $con = mysqli_connect($host_db,$user_db,$pass_db,$nama_db);
 
 mysqli_select_db($con, $nama_db);
